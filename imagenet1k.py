64b00e22128d390c7b722053a2364f4d53fa7a99
edwardyehuang <edward.ye.huang@qq.com>
2026-04-16 05:02:31 +0000

    update imagenet1k

--- imagenet1k.py
+++ imagenet1k.py
@@ -6,7 +6,16 @@ import tensorflow as tf
 from scipy.io import loadmat
 
 from ids.dataset import Dataset
-from iseg.data_process.pipeline import AugmentationsPipeLine
+from iseg.data_process.augments import (
+	CenterCropImageAugment,
+	RandomBrightnessAugment,
+	RandomFlipImageAugment,
+	RandomJEPGQualityAugment,
+	RandomNoisyEvalAugment,
+	RandomResizedCropImageAugment,
+	ResizeShortEdgeImageAugment,
+)
+from iseg.data_process.pipeline import ClassificationAugmentationsPipeLine
 
 
 TRAIN_DIR = "train"
@@ -18,14 +27,14 @@ DEVKIT_META_PATH = os.path.join(DEVKIT_DIR, "data", "meta.mat")
 DEVKIT_VAL_GT_PATH = os.path.join(DEVKIT_DIR, "data", "ILSVRC2012_validation_ground_truth.txt")
 
 
-class ImageNet1KAugmentationsPipeLine(AugmentationsPipeLine):
-	def __init__(self, process_fn, name=None):
-		super().__init__(perform_post_process=False, name=name)
-		self._process_fn = process_fn
-
-	@tf.autograph.experimental.do_not_convert
-	def process(self, image, label):
-		return self._process_fn(image, label)
+class ImageNet1KAugmentationsPipeLine(ClassificationAugmentationsPipeLine):
+	def __init__(self, target_height, target_width, augments=None, name=None):
+		super().__init__(
+			target_height=target_height,
+			target_width=target_width,
+			augments=augments if augments is not None else [],
+			name=name,
+		)
 
 
 class ImageNet1K(Dataset):
@@ -41,6 +50,10 @@ class ImageNet1K(Dataset):
 		self.crop_width = 224
 		self.eval_crop_height = 224
 		self.eval_crop_width = 224
+		self.classification_crop_area_range = (0.2, 1.0)
+		self.classification_crop_aspect_ratio_range = (3.0 / 4.0, 4.0 / 3.0)
+		self.classification_crop_max_attempts = 10
+		self.eval_crop_pct = 0.875
 
 		self._wnid_to_label = None
 		self._train_augmentations_pipeline = None
@@ -211,104 +224,47 @@ class ImageNet1K(Dataset):
 		return self._eval_augmentations_pipeline(ds)
 
 	def create_augment_pipeline(self, training=False):
-		process_fn = self._train_process if training else self._eval_process
+		target_h = self.crop_height
+		target_w = self.crop_width
+
+		if not training:
+			target_h = self.eval_crop_height if self.eval_crop_height is not None else self.crop_height
+			target_w = self.eval_crop_width if self.eval_crop_width is not None else self.crop_width
+
+		augments = []
+
+		if training:
+			augments.append(RandomResizedCropImageAugment(
+				target_height=target_h,
+				target_width=target_w,
+				area_range=self.classification_crop_area_range,
+				aspect_ratio_range=self.classification_crop_aspect_ratio_range,
+				max_attempts=self.classification_crop_max_attempts,
+			))
+
+			if self.random_brightness:
+				augments.append(RandomBrightnessAugment(execute_prob=0.5))
+
+			augments.append(RandomFlipImageAugment(self.prob_of_flip))
+
+			if self.random_jepg_quality:
+				augments.append(RandomJEPGQualityAugment())
+		else:
+			resize_short_edge = int(round(float(min(target_h, target_w)) / float(self.eval_crop_pct)))
+			augments.append(ResizeShortEdgeImageAugment(short_edge=resize_short_edge))
+			augments.append(CenterCropImageAugment(target_h, target_w))
+
+			if self.random_noisy_eval_level > 0 + 1e-3:
+				augments.append(RandomNoisyEvalAugment(noise_level=self.random_noisy_eval_level))
+
 		pipeline_name = "imagenet1k_train_pipeline" if training else "imagenet1k_eval_pipeline"
 
 		return ImageNet1KAugmentationsPipeLine(
-			process_fn=process_fn,
+			target_height=target_h,
+			target_width=target_w,
+			augments=augments,
 			name=pipeline_name,
 		)
-
-	@tf.autograph.experimental.do_not_convert
-	def _train_process(self, image, label):
-		image = self._random_scale_for_train(image)
-		image = self._pad_to_target_size(image, self.crop_height, self.crop_width)
-		image = tf.image.random_crop(image, [self.crop_height, self.crop_width, 3])
-
-		if self.random_brightness:
-			image = tf.image.random_brightness(image, max_delta=32.0)
-
-		if self.prob_of_flip > 0:
-			image = tf.cond(
-				tf.random.uniform([]) <= self.prob_of_flip,
-				lambda: tf.image.flip_left_right(image),
-				lambda: image,
-			)
-
-		image.set_shape([self.crop_height, self.crop_width, 3])
-
-		return image, label
-
-	@tf.autograph.experimental.do_not_convert
-	def _eval_process(self, image, label):
-		target_h = self.eval_crop_height if self.eval_crop_height is not None else self.crop_height
-		target_w = self.eval_crop_width if self.eval_crop_width is not None else self.crop_width
-
-		image = self._resize_for_eval(image, target_h, target_w)
-		image.set_shape([target_h, target_w, 3])
-
-		return image, label
-
-	def _random_scale_for_train(self, image):
-		image_shape = tf.shape(image)
-
-		scale = self._sample_scale()
-
-		scaled_h = tf.cast(tf.cast(image_shape[0], tf.float32) * scale, tf.int32)
-		scaled_w = tf.cast(tf.cast(image_shape[1], tf.float32) * scale, tf.int32)
-
-		scaled_h = tf.maximum(scaled_h, self.crop_height)
-		scaled_w = tf.maximum(scaled_w, self.crop_width)
-
-		image = tf.image.resize(image, [scaled_h, scaled_w], method="bilinear")
-
-		return image
-
-	def _sample_scale(self):
-		raw_min_scale = tf.constant(self.min_scale_factor, dtype=tf.float32)
-		raw_max_scale = tf.constant(self.max_scale_factor, dtype=tf.float32)
-
-		min_scale = tf.minimum(raw_min_scale, raw_max_scale)
-		max_scale = tf.maximum(raw_min_scale, raw_max_scale)
-
-		if self.scale_factor_step_size is None or self.scale_factor_step_size <= 0:
-			return tf.random.uniform([], minval=min_scale, maxval=max_scale)
-
-		step = tf.constant(self.scale_factor_step_size, dtype=tf.float32)
-		num_steps = tf.cast(tf.math.floor((max_scale - min_scale) / step), tf.int32) + 1
-		rand_step = tf.random.uniform([], minval=0, maxval=num_steps, dtype=tf.int32)
-
-		return tf.clip_by_value(min_scale + tf.cast(rand_step, tf.float32) * step, min_scale, max_scale)
-
-	def _pad_to_target_size(self, image, target_h, target_w):
-		image_shape = tf.shape(image)
-		current_h = image_shape[0]
-		current_w = image_shape[1]
-
-		out_h = current_h + tf.maximum(target_h - current_h, 0)
-		out_w = current_w + tf.maximum(target_w - current_w, 0)
-
-		mean_pixel = tf.reshape(tf.cast(self.mean_pixel, image.dtype), [1, 1, 3])
-		image = image - mean_pixel
-		image = tf.image.pad_to_bounding_box(image, 0, 0, out_h, out_w)
-		image = image + mean_pixel
-
-		return image
-
-	def _resize_for_eval(self, image, target_h, target_w):
-		image_shape = tf.shape(image)
-
-		scale_h = tf.cast(target_h, tf.float32) / tf.cast(image_shape[0], tf.float32)
-		scale_w = tf.cast(target_w, tf.float32) / tf.cast(image_shape[1], tf.float32)
-		scale = tf.maximum(scale_h, scale_w)
-
-		resized_h = tf.cast(tf.round(tf.cast(image_shape[0], tf.float32) * scale), tf.int32)
-		resized_w = tf.cast(tf.round(tf.cast(image_shape[1], tf.float32) * scale), tf.int32)
-
-		image = tf.image.resize(image, [resized_h, resized_w], method="bilinear")
-		image = tf.image.resize_with_crop_or_pad(image, target_h, target_w)
-
-		return image
 
 	@tf.autograph.experimental.do_not_convert
 	def _tfrecord_read_map_fn(self, example_proto):
